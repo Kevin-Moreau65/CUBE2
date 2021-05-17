5238f56c399b6a61a225d20611e1cbe08ce69472
prinfray <paul.rinfray@gmail.com>
2021-05-17 18:54:53 +0200

    UP

--- Site web/js/PageFinale.js
+++ Site web/js/PageFinale.js
@@ -83,4 +83,18 @@ $("#Suivant").click(function () {
         $("#AlerteRetour, #Darken").hide();
       }, 100);
     });
-    
+    // INPUT TYPE FILE AFFICHER APPERCU AVANT ENVOI ////////////////////////////////////////
+    function readURL(input) {
+      if (input.files && input.files[0]) {
+          var reader = new FileReader();
+
+          reader.onload = function (e) {
+              $('#blah')
+                  .attr('src', e.target.result)
+                  .width(150)
+                  .height(200);
+          };
+
+          reader.readAsDataURL(input.files[0]);
+      }
+  }

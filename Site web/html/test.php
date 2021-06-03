c8f29ffe2570089d0a7bd8dcc357435c922bd6ef
Kevin-Moreau65 <79045771+Kevin-Moreau65@users.noreply.github.com>
2021-06-03 17:04:04 +0200

    1ere requete php

--- Site web/html/test.php
+++ Site web/html/test.php
@@ -1,2 +1,10 @@
 <?php
-echo $_POST['name'];
+$db = new PDO('mysql:host=localhost;dbname=cesi;charset=utf8', 'root', '');
+$r = "INSERT INTO eleves (nom, prenom, email, datedenaissance, tel) VALUE (:nom,:prenom,:email,:datedeN, :tel)";
+$sql = $db->prepare($r);
+$sql->bindParam(':nom', $_POST["nom"], PDO::PARAM_STR);
+$sql->bindParam(':prenom', $_POST["prenom"], PDO::PARAM_STR);
+$sql->bindParam(':email', $_POST["email"], PDO::PARAM_STR);
+$sql->bindParam(':datedeN', $_POST["datedeN"], PDO::PARAM_STR);
+$sql->bindParam(':tel', $_POST["tel"], PDO::PARAM_STR);
+$resultat = $sql->execute();
